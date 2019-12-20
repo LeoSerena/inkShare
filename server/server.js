ef3817feb5cbfd4742a96579761cbecc6351e9c2
Leo <leo.serena@epfl.ch>
2019-12-20 01:03:18 +0100

    more

---
+++ server/server.js
@@ -0,0 +1,17 @@
+/* names : inkStudies inklassic*/
+
+var express = require('express')
+
+var app = express();
+app.use(express.json());
+app.use(express.static('lib'));
+
+app.get('/index', function(req, res){
+    res.sendFile(__dirname + '/html/index.html')
+})
+
+var server = app.listen(8081, function(){
+    var port = server.address().port;
+    console.log('app listening at port ' + port)
+
+})
