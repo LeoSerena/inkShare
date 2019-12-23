84456283127ecdd54223941e1582c6ae91369d7b
Leo Serena <leoserena@hotmail.ch>
2019-12-23 17:20:56 +0100

    new stuff

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 /* names : inkStudies inklassic*/
 
 var express = require('express');
-
+var path = require('path');
 var app = express();
+app.use(express.static('util'))
+var appDir = path.dirname(require.main.filename);
 app.use(express.json());
-app.use(express.static('lib'));
 
 
 
 app.get('/index', function(req, res){
-    res.sendFile(__dirname + '/html/index.html')
+    res.sendFile(path.resolve(__dirname + '/index.html'))
 })
 
 var server = app.listen(8081, function(){
     var port = server.address().port;
     console.log('app listening at port ' + port)
-
+    console.log('root path: ' + appDir)
 })
---
+++ util/search.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+    $('#submit').click(
+        function(e){
+            e.preventDefault();
+            var search = $('#test').val();
+            console.log(search);
+            if(search == 'bigg yoshi'){
+                console.log('bigg yoshi mode activated')
+                $('body').css({'background-image': 'url("bigg_yoshi.jpg")'});
+            }else{
+                var data = {}
+                data.search = search;
+                $.ajax({
+                    type : 'POST',
+                    url : '/search_query',
+                    data : JSON.stringify(data),
+                    contentType : 'application/json',
+                    sucess : function(response){
+                        display_query(response);
+                    },
+                    error(err){
+                        $('#query_result').text(err);
+                    }
+                })
+            }
+        }
+    )
+
+})
