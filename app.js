2b9f580a5605ea77c5bbb85e5fa8630c205b1014
Leo <leo.serena@epfl.ch>
2019-12-24 10:12:04 +0100

    added 404 page

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var express = require('express');
 var path = require('path');
 var app = express();
 app.use(express.static('util'))
-var appDir = path.dirname(require.main.filename);
 app.use(express.json());
 
 
@@ -13,8 +12,12 @@ app.get('/index', function(req, res){
     res.sendFile(path.resolve(__dirname + '/index.html'))
 })
 
+app.get('/*', function(rew, res){
+    res.sendFile(path.resolve(__dirname + '/404.html'))
+})
+
 var server = app.listen(8081, function(){
     var port = server.address().port;
     console.log('app listening at port ' + port)
-    console.log('root path: ' + appDir)
 })
+
